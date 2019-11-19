948d3e81170230256fffc23f6294bf3e80a4d648
Nazar2a <50366818+Nazar2a@users.noreply.github.com>
2019-11-20 01:42:35 +0200

    divided the project into modules, made moving the pin

---
+++ js/form.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// Модуль, который работает с формой объявления
+(function () {
+
+// Реализация заполнение поля адреса при клике по пину
+  var tagCoords = function (tag, tagStatus) {
+    var box = tag.getBoundingClientRect();
+
+    return {
+      top: (box.top + window.pageYOffset) + tagStatus,
+      left: (box.left + window.pageXOffset) + (Math.floor(window.pins.PIN_MAIN_RADIUS / 2))
+    };
+  };
+
+  var recordCoordsInInput = function (tag, tagStatus, input) {
+    var mapPinMainCoords = tagCoords(tag, tagStatus);
+    document.getElementById(input).value = mapPinMainCoords.top + ', ' + mapPinMainCoords.left;
+  };
+
+  recordCoordsInInput(window.mapPinMain, window.pins.PIN_MAIN_HEIGHT_INACTIVE, 'address');
+
+  window.mapPinMain.addEventListener('mousemove', function () {
+    recordCoordsInInput(window.mapPinMain, window.pins.PIN_MAIN_HEIGHT_ACTIVE, 'address');
+  });
+
+// Программируем сценарий установки соответствия количества гостей с количеством комнат.
+
+  var roomNumbers = document.querySelector('#room_number');
+  var capacity = document.querySelector('#capacity');
+
+  capacity.addEventListener('focus', function () {
+    for (var i = 0; i < capacity.options.length; i++) {
+      capacity.options[i].removeAttribute('disabled', 'disabled');
+    }
+
+    var roomNumber = Number(roomNumbers.value);
+    if (roomNumber === 1) {
+      capacity.options[0].setAttribute('disabled', 'disabled');
+      capacity.options[1].setAttribute('disabled', 'disabled');
+      capacity.options[3].setAttribute('disabled', 'disabled');
+    }
+    if (roomNumber === 2) {
+      capacity.options[0].setAttribute('disabled', 'disabled');
+      capacity.options[3].setAttribute('disabled', 'disabled');
+    }
+    if (roomNumber === 3) {
+      capacity.options[3].setAttribute('disabled', 'disabled');
+    }
+    if (roomNumber === 100) {
+      capacity.options[0].setAttribute('disabled', 'disabled');
+      capacity.options[1].setAttribute('disabled', 'disabled');
+      capacity.options[2].setAttribute('disabled', 'disabled');
+    }
+  });
+
+}) ();
---
+++ js/map.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// переход страници в активное состояние
+(function () {
+  var adForm = document.querySelector('.ad-form');
+  var mapFilters = document.querySelector('.map__filters');
+  var ENTER_KEYCODE = 13;
+
+  var deleteAttributes = function (block, selector, atribute) {
+    var elementsOfBlock = block.querySelectorAll(selector);
+
+    for (var i = 0; i < elementsOfBlock.length; i++) {
+      elementsOfBlock[i].removeAttribute(atribute);
+    }
+  };
+
+  var removeClass = function (selector, classSelector) {
+    document.querySelector(selector).classList.remove(classSelector);
+  };
+
+  var pageActivation = function () {
+    removeClass('.map', 'map--faded');
+    removeClass('.ad-form', 'ad-form--disabled');
+    deleteAttributes(adForm, 'fieldset', 'disabled');
+    deleteAttributes(mapFilters, 'select', 'disabled');
+    deleteAttributes(mapFilters, 'fieldset', 'disabled');
+  };
+
+  window.mapPinMain.addEventListener('mouseup', function () {
+    pageActivation();
+  });
+
+  window.mapPinMain.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      pageActivation();
+    }
+  });
+
+}) ();
